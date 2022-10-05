513d8b1a8d7237809d8bace422afa3f95fa36d1e
dmitryi698 <dima2prog@gmail.com>
2022-10-05 11:35:40 +0400

    Add project settings

---
+++ jest.config.js
@@ -0,0 +1,9 @@
+// For a detailed explanation regarding each configuration property, visit:
+// https://jestjs.io/docs/en/configuration.html
+
+export default {
+	testEnvironment: 'jest-environment-node',
+	transform: {},
+	moduleFileExtensions: [`js`, `jsx`, `ts`, `tsx`, `json`, `node`, `mjs`],
+	testMatch: ['**/__tests__/**/*.[jt]s?(x)', '**/?(*.)+(spec|test).[tj]s?(x)', '**/jest.config.mjs']
+};
