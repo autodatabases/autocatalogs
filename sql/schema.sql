7e8398d14b1c8f1169b5ef17a3d69633b754d84c
MaximumQuiet <contact@maximumquiet.cyou>
2022-11-30 13:06:29 +0400

    Keep all privileges only on autocatalogs

--- sql/schema.sql
+++ sql/schema.sql
@@ -1,5 +1,4 @@
 drop schema if exists autocatalogs;
 create schema autocatalogs;
-grant all privileges on *.* to 'autocatalogs'@'localhost' identified by 'autocatalogs';
 grant all privileges on autocatalogs.* to 'autocatalogs'@'localhost' identified by 'autocatalogs';
 grant all privileges on autocatalogs.* to 'autocatalogs'@'%' identified by 'autocatalogs';
